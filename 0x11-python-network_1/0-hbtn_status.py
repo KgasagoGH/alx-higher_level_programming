79d32f3e423ffccad329527b632646dabd7f2048
KgasagoGH <130768415+KgasagoGH@users.noreply.github.com>
2023-09-29 21:36:25 +0200

    github upload

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,9 @@
-Body response:$
-    - type: <class bytes>$
+#!/usr/bin/python3
+# Task 0. What's my status? #0
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
