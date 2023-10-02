0b27a829f7a19f68e6e19feb848f143baa27a746
KgasagoGH <130768415+KgasagoGH@users.noreply.github.com>
2023-10-02 21:11:51 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL - POST parameters
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
