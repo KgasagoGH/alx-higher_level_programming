4291d4da5e5a042a1ab081d2c7cef29cdc364469
KgasagoGH <130768415+KgasagoGH@users.noreply.github.com>
2023-10-02 21:09:49 +0200

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL - only methods
-curl -sI -X OPTIONS "$1" | grep Allow: | cut -d " " -f 2-
+# cURL - only methods also displays http url
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
